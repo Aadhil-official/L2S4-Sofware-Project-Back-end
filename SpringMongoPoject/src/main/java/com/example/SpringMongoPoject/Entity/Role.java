6dad2620ea5b7d2ce408169ff39be6daff2f10f0
Aadhil-official <aadhilmhm.21@uom.lk>
2024-03-06 01:26:08 +0530

    Add Authorization depandancies and create Role management

---
+++ SpringMongoPoject/src/main/java/com/example/SpringMongoPoject/Entity/Role.java
@@ -0,0 +1,21 @@
+package com.example.SpringMongoPoject.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Role {
+    @Id
+    private String id;
+    private LogRole name;
+
+    public Role(LogRole name) {
+        this.name = name;
+    }
+}
