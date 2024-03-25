1b5892b0a95ba896b8465304e4db29ff7f3d8eb4
Godawela <110478199+Godawela@users.noreply.github.com>
2024-03-25 20:31:30 +0530

    Add files via upload

---
+++ src/main/java/calendar/calendar/EventRepository.java
@@ -0,0 +1,8 @@
+package calendar.calendar;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends MongoRepository<Event, String> {
+}
