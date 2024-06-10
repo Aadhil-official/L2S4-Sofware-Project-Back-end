edbd409743634e1943d6fef005dbc19221bf60ec
Aadhil-official <aadhilmhm.21@uom.lk>
2024-06-10 06:03:58 +0530

    Added Service for testing

---
+++ Software-project/src/main/java/com/example/Software/project/Entity/DTO/ServiceAgrementDTO.java
@@ -0,0 +1,13 @@
+package com.example.Software.project.Entity.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class ServiceAgrementDTO {
+}
