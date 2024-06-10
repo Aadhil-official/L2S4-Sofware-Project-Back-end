54f4f7f3f53b06bb8d743d9a288cd910d332d146
Aadhil-official <aadhilmhm.21@uom.lk>
2024-06-11 03:00:25 +0530

    Delete umesas file

--- Software-project/src/main/java/com/example/Software/project/calenedar/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Software.project.calenedar;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends MongoRepository<Event, String> {
-}
