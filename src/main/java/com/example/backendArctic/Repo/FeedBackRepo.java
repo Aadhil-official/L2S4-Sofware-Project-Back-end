fa3d21cbf0343c046a6580df034196509b00de84
Aadhil-official <aadhilmhm.21@uom.lk>
2024-07-03 22:50:39 +0530

    change insert

--- src/main/java/com/example/backendArctic/Repo/FeedBackRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.backendArctic.Repo;
-
-import com.example.backendArctic.Entity.FeedBackForm;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FeedBackRepo extends MongoRepository<FeedBackForm ,String> {
-}
