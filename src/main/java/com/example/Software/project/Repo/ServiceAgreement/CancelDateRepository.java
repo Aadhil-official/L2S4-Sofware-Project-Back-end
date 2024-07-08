eced8c9d5861bf428e57ff423ac600ccbdd5180e
Aadhil-official <112420417+Aadhil-official@users.noreply.github.com>
2024-07-09 03:39:15 +0530

    Update CancelDateRepository.java

--- src/main/java/com/example/Software/project/Repo/ServiceAgreement/CancelDateRepository.java
+++ src/main/java/com/example/Software/project/Repo/ServiceAgreement/CancelDateRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CancelDateRepository extends MongoRepository<CancelDates,String> {
+public interface CancelDateRepository extends MongoRepository<CancelDates,Long> {
     boolean existsByVisitId(String visitId);
 }
