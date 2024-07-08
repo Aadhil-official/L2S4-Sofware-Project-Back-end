d4d11eb4a185537be89dc75cb2bcbd7d7703013c
Aadhil-official <112420417+Aadhil-official@users.noreply.github.com>
2024-07-09 03:43:12 +0530

    Update EndDateRepository.java

--- src/main/java/com/example/Software/project/Repo/ServiceAgreement/EndDateRepository.java
+++ src/main/java/com/example/Software/project/Repo/ServiceAgreement/EndDateRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EndDateRepository extends MongoRepository<EndDates, String> {
+public interface EndDateRepository extends MongoRepository<EndDates, Long> {
     boolean existsByVisitId(String visitId);
     // You can define custom queries or methods here if needed
 }
